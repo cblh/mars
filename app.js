be1f7db5db12afb99816b35c20de21b3dc7af064
burlin <chenbeiliuhuo@gmail.com>
2015-06-27 13:47:47 +0800

    import model and wechat

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var express = require('express');
+var express = require('express'),
+	Activity = require('./model/activities'),
+	User = require('./model/users'),
+	Record = require('./model/records');
+
+var dbCallback = function (err) {
+	if (err) {
+		console.error.bind(console, 'connection error:');
+  	}else {
+    	console.log('save sucess');	
+  	}
+};
+
 var app = express();
 
 app.get('/app', function (req, res) {
@@ -17,13 +29,17 @@ var server = app.listen(3000, function () {
 
 var wechat = require('wechat');
 var config = {
-  token: 'token',
-  appid: 'appid',
-  encodingAESKey: 'encodinAESKey'
+  token: 'mars',
+  appid: 'wx9fb1b4868ad65f02',
+  encodingAESKey: '49d925d6b41a6fefc32545325d4d98ee5d4d98ee8ee'
 };
 
 app.use(express.query());
 app.use('/wechat', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
+  if(message.signature) {
+    console.log('signature'+query.signature);
+    res.send(message.echostr);
+  };
 }));
