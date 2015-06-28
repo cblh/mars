3c9d318156f158ef19f48b9d1c900088735953c0
burlin <chenbeiliuhuo@gmail.com>
2015-06-28 09:08:06 +0800

    1

--- model/users.js
+++ model/users.js
@@ -10,8 +10,8 @@ db.once('open', function (callback) {
 var User = mongoose.model('User', Schema({
     type: Number,//0,100，administrator
     wechatName: String,
-    openId: Number,//绑定微信账号
     nationId: Number,//身份证，默认null
+    openId: String,//绑定微信账号
     headimgUrl: String,
     nickName: String,
     // records: Array//_recordId:Schema.Types.ObjectId,
